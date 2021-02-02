7cb295a48dd4f218f836738d918093efd80cdaa2
Paul <paul.putegnat@edu.devinci.fr>
2021-02-02 11:31:57 +0100

    First commit

---
+++ POOWeek2 Proj/Assets/Scripts/ApplicationManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ApplicationManager : MonoBehaviour
+{
+    private static ApplicationManager _instance = null;
+
+    public static ApplicationManager GetInstance()
+    {
+        if(null == _instance)
+            _instance = new ApplicationManager();
+
+        return _instance;
+    }
+}
